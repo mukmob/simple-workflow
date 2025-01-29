672af42565799b788b16ad9f5b777905d38e35a5
mukmob <mukumbula.mbombo1212@gmail.com>
2025-01-29 16:31:55 -0400

    Update test and activate logging

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,9 +7,10 @@ logger = logging.getLogger(__name__)
 
 class OdooLoginTest(BaseCase):
     def test_login(self):
+        logging.info("test_login")
         self.open("http://localhost:8069")
-        self.type("#login", "admin")
-        self.type("#password", "admin")
+        self.type("#login", "user@example.com")
+        self.type("#password", "bitnami")
         self.click('button[type="submit"]')
         logger.info(self.get_page_source())
-        self.assert_text("Odoo", "title")
+        self.assert_element("[data-hotkey='h']")
